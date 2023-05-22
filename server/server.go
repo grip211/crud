d9fa42d990cdcd42371343258ae6d8d6d486e1f5
Pavel Grigorev <grip211@mail.ru>
2023-05-22 21:55:24 +0300

    initial commit

---
+++ server/server.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Product struct {
+	Id      int
+	Model   string
+	Company string
+	Price   int
+}
+
+var database *sql.DB
+
+// возвращаем пользователю страницу для редактирования объекта
+func EditPage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	row := database.QueryRow("select * from productdb.Products where id = ?", id)
+	prod := Product{}
+	err := row.Scan(&prod.Id, &prod.Model, &prod.Company, &prod.Price)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, http.StatusText(404), http.StatusNotFound)
+	} else {
+		tmpl, _ := template.ParseFiles("templates/edit.html")
+		tmpl.Execute(w, prod)
+	}
+}
+
+// получаем измененные данные и сохраняем их в БД
+func EditHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+	}
+	id := r.FormValue("id")
+	model := r.FormValue("model")
+	company := r.FormValue("company")
+	price := r.FormValue("price")
+
+	_, err = database.Exec("update productdb.Products set model=?, company=?, price = ? where id = ?",
+		model, company, price, id)
+
+	if err != nil {
+		log.Println(err)
+	}
+	http.Redirect(w, r, "/", 301)
+}
+
+func CreateHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+		model := r.FormValue("model")
+		company := r.FormValue("company")
+		price := r.FormValue("price")
+
+		_, err = database.Exec("insert into productdb.Products (model, company, price) values (?, ?, ?)",
+			model, company, price)
+
+		if err != nil {
+			log.Println(err)
+		}
+		http.Redirect(w, r, "/", 301)
+	} else {
+		http.ServeFile(w, r, "templates/create.html")
+	}
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+
+	rows, err := database.Query("select * from productdb.Products")
+	if err != nil {
+		log.Println(err)
+	}
+	defer rows.Close()
+	products := []Product{}
+
+	for rows.Next() {
+		p := Product{}
+		err := rows.Scan(&p.Id, &p.Model, &p.Company, &p.Price)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		products = append(products, p)
+	}
+
+	tmpl, _ := template.ParseFiles("templates/index.html")
+	tmpl.Execute(w, products)
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	db, err := sql.Open("mysql", "test:changeme123@tcp(194.35.48.20)/pavel")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := db.PingContext(ctx); err != nil {
+		panic(err)
+	}
+
+	database = db
+	defer func() {
+		if err := db.Close(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", IndexHandler)
+	router.HandleFunc("/create", CreateHandler)
+	router.HandleFunc("/edit/{id:[0-9]+}", EditPage).Methods("GET")
+	router.HandleFunc("/edit/{id:[0-9]+}", EditHandler).Methods("POST")
+
+	http.Handle("/", router)
+
+	fmt.Println("Server is listening...")
+	http.ListenAndServe(":8181", nil)
+}
